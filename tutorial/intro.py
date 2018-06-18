15fbbca7040ec833da350dc59b90f0560d1cadb9
Ingrid Sena <job@ingridsena.com.br>
2018-06-18 17:16:15 -0300

    My Django Girls app, first commit

---
+++ tutorial/intro.py
@@ -0,0 +1,11 @@
+a = 'ingrid'
+b = len(a)
+
+
+print ('tentando de len-1 a -1 reverse')
+for i in range(len(a)-1, -1, -1):
+    print (a[i])
+
+print ('tentando de 10 a -1 reverse')
+for i in range(10, -1, -1):
+    print (i)
